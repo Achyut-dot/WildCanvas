cbde1c95a735313d6ed96165383a2f780852a7b1
Achyut Raghuvanshi <achyut24122002@gmail.com>
2025-04-28 10:01:54 +0530

    working on Cart ciew component

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartViewComponent } from './cart-view/cart-view.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -9,7 +11,10 @@ import { CartViewComponent } from './cart-view/cart-view.component';
     CartViewComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule
   ]
 })
 export class CartModule { }
